6d48d0a31b297b0b02e62aa8e56a40ef419e7d85
MrLarkyy_ <studiotadikl@gmail.com>
2026-04-15 14:14:51 +0200

    Update JVM toolchains to version 25 across multiple modules
    Updated the JVM toolchain configuration in various build scripts from version 21 to version 25 to align with the latest requirements. Also simplified a registry-related method to avoid unnecessary casting.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ subprojects {
 
     plugins.withId("org.jetbrains.kotlin.jvm") {
         extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
-            jvmToolchain(21)
+            jvmToolchain(25)
         }
     }
 
