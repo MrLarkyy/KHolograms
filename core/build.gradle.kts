6d6235e9d9ebafbd475bb9d7b7635ace013a19aa
MrLarkyy_ <studiotadikl@gmail.com>
2026-06-17 00:57:47 +0200

    Update dependencies and Kotlin version in build scripts
    Bump `gg.aquatic:Common` to `26.0.17` and `org.jetbrains.kotlinx:kotlinx-coroutines-core` to `1.11.0`. Update Kotlin plugin version to `2.4.0`.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    api("gg.aquatic:Common:26.0.16") {
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    api("gg.aquatic:Common:26.0.17") {
         isChanging = true
     }
     api("gg.aquatic.replace:Replace:26.0.3")
